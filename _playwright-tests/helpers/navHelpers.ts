7e569ddaeace934b72bda159971302e3fe84bead
Adarsh Dubey <addubey@redhat.com>
2025-08-19 13:01:47 +0530

    title and timer

--- _playwright-tests/helpers/navHelpers.ts
+++ _playwright-tests/helpers/navHelpers.ts
@@ -2,6 +2,7 @@ import { type Page } from '@playwright/test';
 import { test, expect } from '@playwright/test';
 
 export const navigateToInventoryFunc = async (page: Page) => {
+  //title
   await page.goto('/insights/inventory/', { timeout: 100000 });
   await expect(page.getByRole('heading', { name: 'Systems' })).toBeVisible({ timeout: 100000 });
 };
